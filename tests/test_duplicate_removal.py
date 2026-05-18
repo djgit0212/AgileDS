4835467674dfa9e8cdc66c6dbdc0f8a3598f53ba
djgit0212 <dengjian@graduate.utm.my>
2026-05-18 12:14:50 +0800

    Update test_duplicate_removal.py

--- tests/test_duplicate_removal.py
+++ tests/test_duplicate_removal.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath("src"))
 from duplicate_removal import remove_duplicates
 
 def test_remove_duplicates():
-   input_file = "data/raw_dataset.csv"
+   input_file = "data/dataset.csv"
    output_file = "data/processed_dataset.csv"
    cleaned_df = remove_duplicates(input_file, output_file)
    original_df = pd.read_csv(input_file)
