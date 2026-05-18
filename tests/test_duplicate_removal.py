db140a022c14484411e0faa0a7a0ebac0b4aa6b7
djgit0212 <dengjian@graduate.utm.my>
2026-05-18 11:31:13 +0800

    Create test_duplicate_removal.py

---
+++ tests/test_duplicate_removal.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from src.duplicate_removal import remove_duplicates
+def test_remove_duplicates():
+   input_file = "data/raw_dataset.csv"
+   output_file = "data/processed_dataset.csv"
+   cleaned_df = remove_duplicates(input_file, output_file)
+   original_df = pd.read_csv(input_file)
+   assert len(cleaned_df) <= len(original_df)
